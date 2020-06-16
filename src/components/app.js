82c067099cb94e6d461e23154de010de7af08b2c
Ballred95 <blake.allred95@icloud.com>
2020-06-15 18:48:34 -0600

    added functions

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from './portfolio/portfolio-container'
+import portfolioItem from './portfolio/portfolio-item';
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Blake Allred Portfolio</h1>
-        <h2>React Redux Router</h2>
         <div>{moment().format("dddd, MMMM Do YYYY, h:mm:ss a")}</div>
+        <PortfolioContainer/>
+        
       </div>
     );
   }
