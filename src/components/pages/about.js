7c31da63885892b524b942f72818432aad09b189
Ballred95 <blake.allred95@icloud.com>
2020-08-05 11:05:48 -0600

    Added About me section

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Facepic from '../0.jpg'
 
 export default function() {
     return(
-        <div>
-            About
+        <div className='about-container'>
+            <img src={Facepic}/>
+            <div className='p-container'>
+            <p>
+            Eager to improve and a lifelong learner. Trained in both front-end and back-end technologies to bring a Fullstack approach to any team, project or company. I'm an avid enthusiast for anything I find intriguing or a cause I'm passionate about, and am absolutely enthralled with the power of code and what can be done with it. ENFP personality type through and through, stubbornly determined to solve problems worth solving, love working on projects that serve a higher purpose, are a challenge, tell a creative story, or just look plain cool. I've studied in fields such as Digital Cinema, 3D animation, modeling/game design, and have settled on using my strengths and talents in the field of Web Design and Development. Bottegga Code School Graduate, pursuing degree in Web and App development at UVU.
+            </p>
+            
+            </div>
         </div>
     ) 
 }
